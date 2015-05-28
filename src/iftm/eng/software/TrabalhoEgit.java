98aca20569c52a7f36c20efdbf6e271fb7695734
adriano <mcway@bol.com.br>
2015-05-28 20:17:02 -0300

    Commit inicial  Primeira linha

---
+++ src/iftm/eng/software/TrabalhoEgit.java
@@ -0,0 +1,12 @@
+package iftm.eng.software;
+
+public class TrabalhoEgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primeira linha do trabalho");
+		System.out.println("Segunda linha do trabalho");
+		
+	}
+
+}
