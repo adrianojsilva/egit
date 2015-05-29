0fb95ccc2f6ff6870cff085e40bfeb4359acfd5c
Vinicius <Vinicius@vinicius-pc>
2015-05-28 21:30:53 -0300

    vinissao

--- src/iftm/eng/software/TrabalhoEgit.java
+++ src/iftm/eng/software/TrabalhoEgit.java
@@ -4,9 +4,9 @@ public class TrabalhoEgit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Primeira linha do trabalho");
-		System.out.println("Segunda linha do trabalho");
-		System.out.println("Terceira linha do trabalho");
+		System.out.println("o japa é um cadeirante");
+		System.out.println("o japa é meu cunhado");
+		System.out.println("o japa é preguicoso");
 		System.out.println("Helton passou aqui...");
 		System.out.println("Helton passou...");
 		System.out.println("Helton passou aqui...");
