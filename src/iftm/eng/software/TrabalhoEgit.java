37cd47c2130375802ba9e30b0c0ac9bd9504786a
Vinicius <Vinicius@vinicius-pc>
2015-05-28 21:33:51 -0300

    garrafa

--- src/iftm/eng/software/TrabalhoEgit.java
+++ src/iftm/eng/software/TrabalhoEgit.java
@@ -6,7 +6,7 @@ public class TrabalhoEgit {
 		// TODO Auto-generated method stub
 		System.out.println("o japa é um cadeirante");
 		System.out.println("o japa é meu cunhado");
-		System.out.println("o japa é preguicosooo");
+		System.out.println("o japa é preguicoso!!!");
 		System.out.println("Helton passou aqui...");
 		System.out.println("Helton passou...");
 		System.out.println("Helton passou aqui...");
